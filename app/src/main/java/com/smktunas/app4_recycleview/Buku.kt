8c9eca418234ad7e0ebce9b4d1a1f41df542119b
dafairpanas <dafapffic@gmail.com>
2025-08-20 10:28:46 +0700

    step 2, step 3, detail activity

--- app/src/main/java/com/smktunas/app4_recycleview/Buku.kt
+++ app/src/main/java/com/smktunas/app4_recycleview/Buku.kt
@@ -2,8 +2,9 @@ package com.smktunas.app4_recycleview
 
 
 data class Buku(
+    val id: Int,
     val judul: String,
     val penulis: String,
-    val tahun: String
-
+    val tahun: String,
+    val cover : String
 )
